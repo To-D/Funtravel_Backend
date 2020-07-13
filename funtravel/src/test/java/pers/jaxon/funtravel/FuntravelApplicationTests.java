cc4cf57ea219f011375f94502b2c5424698c368f
To-D <1244467220@qq.com>
2020-07-14 00:12:58 +0800

    [New] First push

---
+++ funtravel/src/test/java/pers/jaxon/funtravel/FuntravelApplicationTests.java
@@ -0,0 +1,13 @@
+package pers.jaxon.funtravel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FuntravelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
